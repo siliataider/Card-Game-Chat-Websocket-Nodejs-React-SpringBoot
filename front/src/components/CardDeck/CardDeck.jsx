6f4e1d9c3fbb65d38bdc0fc40f5941b7b633d6c3
Silia TAIDER <silia.taider@tpb08.cpe.fr>
2023-12-20 16:35:21 +0100

    update

--- front/src/components/CardDeck/CardDeck.jsx
+++ front/src/components/CardDeck/CardDeck.jsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { setSelectedCards } from '../../slices/gameSlice';
 import Card from '../Card/Card';
 import { getUserCards } from '../../assets/utility'
+import SocketContext from '../../SocketContext';
 
 
-
-const CardDeck = ({ onStartGame }) => {
+const CardDeck = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const userCards = useSelector((state) => state.auth.userCards); 
     const selectedCards = useSelector((state) => state.game.selectedCards);
     const currentUserId = useSelector((state) => state.auth.currentUserId);
+    const { socket } = useContext(SocketContext);
 
     useEffect(() => {
         if (currentUserId) {
           getUserCards(dispatch, currentUserId);
         }
     }, [currentUserId, dispatch]);
-
 
     const toggleCardSelection = (card) => {
         let updatedSelectedCards;
@@ -26,8 +28,27 @@ const CardDeck = ({ onStartGame }) => {
         } else if (selectedCards.length < 5) {
           updatedSelectedCards = [...selectedCards, card];
         }
+        console.log("+++++++++++++ updatedSelectedCards ", updatedSelectedCards)
         dispatch(setSelectedCards(updatedSelectedCards));
     };
+
+    const handleStartGame = () => {
+        if (socket) {
+            const data_to_fight = {
+                fromSocketID: data.fromSocketID,
+                fromID: data.fromID,
+                toSocketID: data.toSocketID,
+                toID: currentUserId,
+            }
+            socket.emit("readyToFight", data)
+        }
+        //navigate('/arena');
+    }
+
+    socket.on('readyToFight', () => {
+        console.log("+++++++++++++++++++ readyToFight")
+        navigate('/cardDeck');
+    });
 
     return (
         <div>
@@ -41,7 +62,7 @@ const CardDeck = ({ onStartGame }) => {
             ))}
         </div>
         {selectedCards.length > 0 && (
-            <button onClick={() => onStartGame(selectedCards)}>Start Game</button>
+            <button onClick={() => handleStartGame(selectedCards)}>Start Game</button>
         )}
         </div>
     );
